5c6d64bf800348f5f8cb1ac0a01f7627b8fdde2a
gbaref949 <110576775+gbaref949@users.noreply.github.com>
2022-10-18 18:26:44 -0700

    red

---
+++ index.js
@@ -0,0 +1,14 @@
+// alert(2==2) //true
+
+// alert(5>5)//false
+
+// alert(4<=5)//true
+
+console.log("w" < "W")
+console.log("w" > "W")
+console.log("w" == "W")
+
+console.log(5 > 4)
+console.log("apple" > "appear")
+console.log("west" < "Went")
+console.log(2 == "12")
